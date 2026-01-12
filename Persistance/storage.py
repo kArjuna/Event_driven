048c48f2b24a1b319cc6a6a858e47482f9b8a9d2
Mallikarjuna Kammari <51478658+kArjuna@users.noreply.github.com>
2026-01-12 17:09:47 -0500

    Add files via upload

---
+++ Persistance/storage.py
@@ -0,0 +1,22 @@
+from business.model.user import User
+
+
+class UserStorage:
+
+    def add(self, user: User) -> int:
+        raise NotImplementedError()
+
+    def get(self, user_id: int) -> User:
+        raise NotImplementedError()
+    
+    def delete(self, user_id: int):
+        raise NotImplementedError()
+    
+    def update(self, user: User):
+        raise NotImplementedError()
+
+    def find_by_user_name(self, user_name: str) -> User:
+        raise NotImplementedError()
+    
+    def get_all_users(self) -> list:
+        raise NotImplementedError()
